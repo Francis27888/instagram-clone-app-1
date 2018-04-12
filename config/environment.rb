09cce7dcc46b4ed5361d0150207e74747b312c5e
Kalisa athanase <kalisamukwiye@gmail.com>
2018-04-12 08:55:35 +0000

    Adding sendgrid credentials in enviromnent.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey',
+  :password => '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',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
